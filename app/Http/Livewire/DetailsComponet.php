e33fff9a3074fc05ac2d4e296728caec63a3912e
Eduardo Teixeira <eduardo.febr@gmail.com>
2022-05-01 17:44:46 -0300

    Segundo commit

---
+++ app/Http/Livewire/DetailsComponet.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DetailsComponet extends Component
+{
+    
+    public function render()
+    {
+        return view('livewire.datails-componet');
+    }
+}
---
+++ app/Http/Livewire/ShopComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShopComponent extends Component
+{
+    use WithPagination;
+    public function render()
+    {
+        $product = Product::paginate(12);
+        return view('livewire.shop-component',['products'=>$product])->layout('layouts.base');
+    }
+}
